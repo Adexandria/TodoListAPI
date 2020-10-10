96445b29d0a810075e2c11101dd954b41aee2b30
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-10-10 09:41:35 +0100

    Added Entity and Services

---
+++ Todo/Services/ITodo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Entity;
+
+namespace Todo.Services
+{
+    public interface ITodo
+    {
+        //Get tasks
+        IEnumerable<Tasks> GetTasks{ get; }
+
+        //Get an Individual by id
+        Task<Tasks> GetTask(Guid id);
+
+        //add task
+        Task<Tasks> Add(Tasks task);
+
+        //update task
+        Task<Tasks> Update(Tasks task);
+
+        //Delete task by id
+        Task<int> Delete(Guid id);
+
+        //saves task
+        Task<int> SaveChanges();
+    }
+}
---
+++ Todo/Services/TodoDb.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Services
+{
+    public class TodoDb :DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+        {
+
+        }
+    }
+}
