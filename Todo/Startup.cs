62146cf2fd05dbe1d682853b76210f8a2fccce01
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-10-10 10:31:49 +0100

    Added Connection string

--- Todo/Startup.cs
+++ Todo/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Todo.Services;
 
 namespace Todo
 {
@@ -26,6 +28,12 @@ namespace Todo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<TodoDb>(opts=> 
+            { 
+                opts.UseSqlServer(Configuration.GetConnectionString("TodoList"));
+            });
+            services.AddScoped<ITodo, TodoRespository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
