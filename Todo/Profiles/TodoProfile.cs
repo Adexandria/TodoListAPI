a4392571799159f5eb8cbfe5f6185ae0cb69c956
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-10-12 08:28:56 +0100

    Added the map profiles

---
+++ Todo/Profiles/TodoProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Entity;
+using Todo.ModelView;
+
+namespace Todo.Profiles
+{
+    public class TodoProfile :Profile
+    {
+        public TodoProfile()
+        {
+            CreateMap<Tasks, TasksDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(dest => dest.Task, opt => opt.MapFrom(s => s.Task));
+
+            CreateMap<Tasks, TaskDto>()
+                .ForMember(dest => dest.Task, opt => opt.MapFrom(s => s.Task));
+
+            CreateMap<TaskCreateDto, Tasks>().ForMember(dest => dest.Task, opt => opt.MapFrom(s => s.Task));
+
+        }
+    }
+}
